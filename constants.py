25d4efb6f3e440117292e7d1cf6b637751b12c74
skiss10 <31713441+skiss10@users.noreply.github.com>
2023-01-23 17:15:47 -0500

    added global constants file

---
+++ constants.py
@@ -0,0 +1,12 @@
+"""
+Module to store all constant
+
+Author: Stephen Kiss (stephenkiss986@gmail.com)
+Date: 01/23/2023
+"""
+
+# This variable is a global variable for controlling the logging level
+LOG_LEVEL = 'DEBUG'
+
+# This variable is a global variable for the path of the log file 
+LOG_FILE_PATH = './app.log'
