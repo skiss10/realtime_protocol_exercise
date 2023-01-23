a73eec600a2e6089c7e2f513ebd24fe45f0569d0
skiss10 <31713441+skiss10@users.noreply.github.com>
2023-01-23 16:17:48 -0500

    create client.py

---
+++ client.py
@@ -0,0 +1,23 @@
+import socket
+import sys
+import uuid
+import random
+
+# get the port number and number of messages from command line
+port = int(sys.argv[1])
+n = int(sys.argv[2]) if len(sys.argv) > 2 else random.randint(1, 0xffff)
+
+# generate a random uuid client identifier
+client_id = str(uuid.uuid4())
+
+# create a socket object
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect to the server
+client_socket.connect(('localhost', port))
+
+# send the client id and number of messages to the server
+client_socket.sendall(f"{client_id} {n}".encode())
+
+# close the connection
+client_socket.close()
