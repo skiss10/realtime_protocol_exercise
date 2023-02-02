3a753829c6a8464feb841755ba4fc3fb186e6537
skiss10 <31713441+skiss10@users.noreply.github.com>
2023-02-01 23:23:54 -0500

    sending heartbeats

--- server.py
+++ server.py
@@ -5,16 +5,17 @@ import logging
 import pickle
 import struct
 import asyncio
+
 from _thread import start_new_thread
 from utils.message import Message
 from utils.checksum import calculate_checksum
 from constants import HEARTBEAT_INTERVAL, LOG_LEVEL, LOG_FILE_PATH
 
-async def send_heartbeats(incoming_message, client_socket, server_name):
+async def send_heartbeats(client_socket, server_name):
     """
     Send heartbeats over client_socket
     """
-    while True: 
+    while True:
         try:
             server_timestamp = str(time.time())
             heartbeat = Message("hearbeat", server_timestamp, server_name)
@@ -23,17 +24,17 @@ async def send_heartbeats(incoming_message, client_socket, server_name):
             serialized_heartbeat = pickle.dumps(heartbeat)
 
             # send stream payload message
-            logging.info("Server - Sending heartbeat to client: %s" % incoming_message.client_id)
+            logging.info("Server - Sending heartbeat to client: %s" % client_socket)
             client_socket.sendall(serialized_heartbeat)
             await asyncio.sleep(HEARTBEAT_INTERVAL)
 
         except socket.error as err:
             logging.info("Server - connection error: {}".format(err))
-            logging.info("Server - stopping hearbeats to %s" % incoming_message.client_id)
+            logging.info("Server - stopping hearbeats to %s" % client_socket)
             break
 
 async def send_payload(incoming_message, client_socket, server_name):
-        # make incrementing uint34 list of messages
+    # make incrementing uint34 list of messages
     sequence_length = incoming_message.data
     logging.info("Server - sequence length requested: %s", sequence_length)
     uint32_numbers = [struct.pack('>I', num) for num in range(1, sequence_length+1)]
@@ -48,12 +49,12 @@ async def send_payload(incoming_message, client_socket, server_name):
         #serialize message
         serialized_message = pickle.dumps(message)
 
-    # send stream payload message
-    logging.info("Server - Sending: " + str(num) + " to " + str(incoming_message.client_id))
-    client_socket.sendall(serialized_message)
-    await asyncio.sleep(1)
+        # send stream payload message
+        logging.info("Server - Sending: " + str(num) + " to " + str(incoming_message.client_id))
+        client_socket.sendall(serialized_message)
+        await asyncio.sleep(1)
 
-        # get the hexadecimal representation of the md5 hash
+    # get the hexadecimal representation of the md5 hash
     checksum = calculate_checksum(uint32_numbers)
     logging.info("Server - calculated checksum: %s" , checksum)
 
@@ -72,9 +73,7 @@ async def message_handler(incoming_message, client_socket, server_name):
     Handle incoming messages
     """
     if incoming_message.name == "greeting":
-        # initiate heartbeats from server to client
-        asyncio.create_task(send_heartbeats(incoming_message, client_socket, server_name))
-        asyncio.create_task(send_payload(incoming_message, client_socket, server_name))
+        await send_payload(incoming_message, client_socket, server_name)
 
 async def client_handler(client_socket, client_address, server_name):
     """
@@ -92,7 +91,7 @@ async def client_handler(client_socket, client_address, server_name):
     incoming_message = pickle.loads(serialized_message)
     logging.info("Server - message recieved of type: %s", incoming_message.name)
 
-    # handle message
+    # handle incoming messages
     await message_handler(incoming_message, client_socket, server_name)
 
 def start_server(port):
@@ -116,6 +115,14 @@ def start_server(port):
 
     return server_socket
 
+async def run_client_handler(client_socket, client_address, server_name):
+    logging.debug("Server - starting client handler for %s" % client_socket)
+    logging.debug("Server - Starting to send heartbeats to %s" % client_socket) 
+    await asyncio.gather(client_handler(client_socket, client_address, server_name), send_heartbeats(client_socket, server_name))
+
+def add_new_client(client_socket, client_address, server_name):
+    asyncio.run(run_client_handler(client_socket, client_address, server_name))
+
 async def main():
     #define server name
     server_name = "server-01"
@@ -133,15 +140,16 @@ async def main():
     port = int(sys.argv[1])
 
     # start the server
-    server_socket = a(port)
+    server_socket = start_server(port)
 
     try:
         while True:
             # establish a connection
             client_socket, client_address = server_socket.accept()
+            logging.info("Server - incoming client socket %s" % client_socket)
 
             # Start a new thread for each client
-            await asyncio.create_task(client_handler(client_socket, client_address, server_name))
+            start_new_thread(add_new_client, (client_socket, client_address, server_name))
 
     except KeyboardInterrupt:
         server_socket.close()
