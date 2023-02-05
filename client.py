ab07ae5b78b9e90717d31c583e6abccad8607560
skiss10 <31713441+skiss10@users.noreply.github.com>
2023-02-05 04:22:32 -0500

    updated logging and naming

--- client.py
+++ client.py
@@ -18,12 +18,12 @@ def compare_stream_hash(uint32_numbers, incoming_message): #TODO, make test to e
     """
     Compare local and recieved hash of uint32 sequence
     """
-    logging.info("[%s] Client - uint32 numbers recieved from server are %s", CLIENT_ID, uint32_numbers)
+    logging.info("[%s] Client - Message - uint32 numbers recieved from server are %s", CLIENT_ID, uint32_numbers)
     # compare checksums
     server_checksum = incoming_message.data
     client_checksum = calculate_checksum(uint32_numbers)
-    logging.info("[%s] Client - checksum recieved from server: %s", CLIENT_ID, server_checksum)
-    logging.info("[%s] Client - calculated checksum from client: %s", CLIENT_ID, client_checksum)
+    logging.info("[%s] Client - Message - checksum recieved from server: %s", CLIENT_ID, server_checksum)
+    logging.info("[%s] Client - Message - calculated checksum from client: %s", CLIENT_ID, client_checksum)
 
     if server_checksum == client_checksum:
         logging.info("[%s] Client - PASS - Checksums from client and Server are the same", CLIENT_ID)
@@ -42,11 +42,11 @@ def message_handler(client_socket, incoming_message, uint32_numbers):
         compare_stream_hash(uint32_numbers, incoming_message)
 
         # close the connection
-        logging.info("[%s] Client - closing connection", CLIENT_ID)
+        logging.info("[%s] Client - Socket - closing connection", CLIENT_ID)
         client_socket.close()
 
     elif incoming_message.name == "heartbeat":
-        logging.info("[%s] Client - recieved heartbeat from server %s", CLIENT_ID, incoming_message.sender_id)
+        logging.info("[%s] Client - Heartbeat - recieved heartbeat from server %s", CLIENT_ID, incoming_message.sender_id)
 
     else:
         logging.info("[%s] Client - message_handler recieved unknown message name type. Discarding message...", CLIENT_ID)
@@ -62,7 +62,7 @@ def server_handler(client_socket, uint32_numbers):
 
                 # unserialize message
                 incoming_message = pickle.loads(serialized_message)
-                logging.info("[%s] Client - message type recieved: %s", CLIENT_ID, incoming_message.name)
+                logging.info("[%s] Client - Message - message type recieved: %s", CLIENT_ID, incoming_message.name)
 
                 # send to incoming message handler
                 message_handler(client_socket, incoming_message, uint32_numbers)
@@ -80,7 +80,7 @@ def format_greeting(sequence_length, client_id):
 
     # format introduction message
     message = Message("greeting", sequence_length, client_id)
-    logging.info("[%s] Client - requesting sequence of length: %s", CLIENT_ID, sequence_length)
+    logging.info("[%s] Client - Message - requesting sequence of length: %s", CLIENT_ID, sequence_length)
 
     # serialize message
     serialized_message = pickle.dumps(message)
@@ -88,7 +88,7 @@ def format_greeting(sequence_length, client_id):
     #return serialized message
     return serialized_message
 
-def connect_to_server(host_ip, port):
+def connect_to_socket(host_ip, port):
     """
     Connect to server and return the socket
     """
@@ -102,15 +102,15 @@ def connect_to_server(host_ip, port):
     while not connected:
         try:
             # connect to the server
-            logging.info("[%s] Client - attempting to connect to server...", CLIENT_ID)
+            logging.info("[%s] Client - Socket - attempting to connect to server...", CLIENT_ID)
             client_socket.connect((host_ip, port))
 
             # set the connected flag to True
             connected = True
 
         except socket.error as err:
-            logging.info("[%s] Client - connection error: %s", CLIENT_ID, err)
-            logging.info("[%s] Client - retrying connection in 15 seconds...", CLIENT_ID)
+            logging.info("[%s] Client - Socket - connection error: %s", CLIENT_ID, err)
+            logging.info("[%s] Client - Socket - retrying connection in 15 seconds...", CLIENT_ID)
             time.sleep(15)
 
     return client_socket
@@ -143,13 +143,13 @@ def main():
     host_ip = 'localhost'
 
     # connect to server
-    client_socket = connect_to_server(host_ip, port)
+    client_socket = connect_to_socket(host_ip, port)
 
     # create greeting message for server
     serialized_greeting = format_greeting(sequence_length, client_id)
 
     # send a message to the server
-    logging.info("[%s] Client - sending greeting message", CLIENT_ID)
+    logging.info("[%s] Client - Message - sending greeting message", CLIENT_ID)
     client_socket.sendall(serialized_greeting)
 
     server_handler(client_socket, uint32_numbers)
