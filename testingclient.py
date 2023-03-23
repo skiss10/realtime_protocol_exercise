ce748ebf367645c2d320482ce71cf492f2d184ec
skiss10 <31713441+skiss10@users.noreply.github.com>
2023-03-23 18:53:33 -0400

    refactored and moved reconnection

--- testingclient.py
+++ testingclient.py
@@ -20,17 +20,23 @@ def send_message(conn):
         conn.send(message.encode('utf-8'))
 
 @exception_handler
-def disconnect(conn, time_until_disconnect, disconnect_duration, host, port):
+def disconnect(conn, host, port):
+
+    time_until_disconnect = None if len(sys.argv) < 2 else int(sys.argv[1])
+    disconnect_duration = None if len(sys.argv) < 2 else int(sys.argv[2])
+
     if time_until_disconnect is not None:
         time.sleep(time_until_disconnect)
         conn.close()
         time.sleep(disconnect_duration)
+        server_handler(host, port, False)
+    else:
+        pass
 
+def server_handler(host, port, sim_discconect = True):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((host, port))
-            print("reconnecting to server")
-
-            s.send("reconnect_message".encode('utf-8'))
+            print("Connected to server")
 
             # spawn a new thread to receive heartbeats from the server
             heartbeat_thread = threading.Thread(target=receive_message, args=(s,))
@@ -40,38 +46,21 @@ def disconnect(conn, time_until_disconnect, disconnect_duration, host, port):
             message_thread = threading.Thread(target=send_message, args=(s,))
             message_thread.start()
 
+            if sim_discconect:
+                # spawn a new thread to disconnect from to the server
+                disconnect_thread = threading.Thread(target=disconnect, args=(s, host, port))
+                disconnect_thread.start()
+                disconnect_thread.join()
+
             heartbeat_thread.join()
             message_thread.join()
-
-    else:
-        pass
 
 @exception_handler
 def main():
     HOST = 'localhost'  # The server's hostname or IP address
     PORT = 12331  # The port used by the server
-    SECONDS_UNTIL_DISCONNECT = None if len(sys.argv) < 2 else int(sys.argv[1])
-    SECONDS_OF_DISCONNECT = None if len(sys.argv) < 2 else int(sys.argv[2])
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        print("Connected to server")
-
-        # spawn a new thread to receive heartbeats from the server
-        heartbeat_thread = threading.Thread(target=receive_message, args=(s,))
-        heartbeat_thread.start()
-
-        # spawn a new thread to send messages to the server
-        message_thread = threading.Thread(target=send_message, args=(s,))
-        message_thread.start()
-
-        # spawn a new thread to send messages to the server
-        disconnect_thread = threading.Thread(target=disconnect, args=(s,SECONDS_UNTIL_DISCONNECT,SECONDS_OF_DISCONNECT, HOST, PORT))
-        disconnect_thread.start()
-
-        heartbeat_thread.join()
-        message_thread.join()
-        disconnect_thread.join()
+    server_handler(HOST, PORT)
 
 if __name__ == "__main__":
     main()
