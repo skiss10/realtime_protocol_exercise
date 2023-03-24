7dc0a41ad859fbdf7dab7b45def2e837a651e7b5
skiss10 <31713441+skiss10@users.noreply.github.com>
2023-03-24 16:23:06 -0400

    stopping threads, checking HB, adding recconect

--- testingclient.py
+++ testingclient.py
@@ -7,35 +7,46 @@ from utils.exception_handler import exception_handler
 
 LAST_HEARTBEAT = time.time()
 
-@exception_handler
-def receive_message(conn, host, port):
+def receive_message(conn, host, port, stop_threads):
     lock = threading.Lock()
-    heartbeat_thread = threading.Thread(target=receive_heartbeat, args=(conn, lock))
-    disconnect_thread = threading.Thread(target=check_disconnect, args=(conn, lock, host, port))
+    heartbeat_thread = threading.Thread(target=receive_heartbeat, args=(conn, lock, stop_threads,))
+    disconnect_thread = threading.Thread(target=check_heartbeat, args=(conn, lock, host, port, stop_threads,))
     heartbeat_thread.start()
     disconnect_thread.start()
     heartbeat_thread.join()
     disconnect_thread.join()
 
-@exception_handler
-def receive_heartbeat(conn, lock):
+
+def receive_heartbeat(conn, lock, stop_threads):
     global LAST_HEARTBEAT
-    while True:
-        data = conn.recv(1024)
-        if data.decode('utf-8') == "Heartbeat":
-            print(f"Received heartbeat at {time.time()}")
-            conn.send(b"Heartbeat_ack")
-            with lock:
-                LAST_HEARTBEAT = time.time()
+    while not stop_threads.is_set():
+        try:
+            data = conn.recv(1024)
+            if data.decode('utf-8') == "Heartbeat":
+                print(f"Received heartbeat at {time.time()}")
+                conn.send(b"Heartbeat_ack")
+                with lock:
+                    LAST_HEARTBEAT = time.time()
+        except OSError:
+            print("Error reading from Socket")
+    print("recieved heartbeats stopped")
 
+def attempt_reconnection(host,port):
+    try:
+        server_handler(host, port)
+    except OSError:
+        print("unable to reconnect to server, trying again in 10 seconds...")
+        time.sleep(10)
+        attempt_reconnection(host,port)
 
-@exception_handler
-def check_disconnect(conn, lock, host, port):
+def check_heartbeat(conn, lock, host, port, stop_threads):
     global LAST_HEARTBEAT
     disconnect_flag = False
     while True:
         if disconnect_flag:
-            disconnect(conn, host, port)
+            conn.close()
+            stop_threads.set()
+            attempt_reconnection(host,port)
             break
         with lock:
             current_time = time.time()
@@ -44,39 +55,42 @@ def check_disconnect(conn, lock, host, port):
                 disconnect_flag = True
         time.sleep(1)
 
-@exception_handler
-def send_message(conn):
-    while True:
-        message = input("Enter message to send: ")
-        conn.send(message.encode('utf-8'))
-
-@exception_handler
-def disconnect(conn, host, port):
-    conn.close()
-    server_handler(host, port)
+def send_message(conn, stop_threads):
+    conn.send(b"Greetings from the client!")
+    print("Begin typing your messages to the server!")
+    while not stop_threads.is_set():
+        try:
+            message = input("Enter message to send: ")
+            conn.send(message.encode('utf-8'))
+        except OSError:
+            print("Unable to send messages over the socket")
+    print('Message Sender Stopped')
 
 def server_handler(host, port):
+        stop_threads = threading.Event()
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((host, port))
             print("Connected to server")
 
             # spawn a new thread to receive heartbeats from the server
-            heartbeat_thread = threading.Thread(target=receive_message, args=(s,host,port,))
+            heartbeat_thread = threading.Thread(target=receive_message, args=(s,host,port,stop_threads,))
             heartbeat_thread.start()
 
             # spawn a new thread to send messages to the server
-            message_thread = threading.Thread(target=send_message, args=(s,))
+            message_thread = threading.Thread(target=send_message, args=(s,stop_threads,))
             message_thread.start()
 
             heartbeat_thread.join()
             message_thread.join()
 
-@exception_handler
 def main():
     HOST = 'localhost'  # The server's hostname or IP address
     PORT = 12331  # The port used by the server
 
-    server_handler(HOST, PORT)
+    try:
+        server_handler(HOST, PORT)
+    except OSError:
+        print("Unable to connect to server.")
 
 if __name__ == "__main__":
     main()
