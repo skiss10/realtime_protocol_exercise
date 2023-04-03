e9986a8e8e625ef3ecca36dbaeed0fa0385e1811
skiss10 <31713441+skiss10@users.noreply.github.com>
2023-04-03 10:45:52 -0700

    updated the connection object

--- utils/connection.py
+++ utils/connection.py
@@ -2,31 +2,29 @@
 Module to define a connection object
 """
 
-# TODO Implement these properties for the connection: https://ably.com/docs/api/realtime-sdk/connection?lang=nodejs
-
-# TODO: Implement methods to run upon initialization of Connection object  
+import time
+import threading
+import uuid
 
 class Connection:
     """
     Define a Connection object
     """
-    def __init__(self, conn):
+    def __init__(self, conn, addr):
         """
         Initialize connection attributes
         """
         self.conn = conn
-        self.id = None
-        self.addr = None
-        self.connection_thread_stopper = None
-        self.last_heartbeat_ack = None
+        self.id = str(uuid.uuid4())
+        self.addr = addr
+        self.connection_thread_stopper = threading.Event()
+        self.last_heartbeat_ack = time.time()
         self.state = None # initial_connection, disconnected, reconnected
         self.client_id = None
-        self.session_storage = None
-        self.threading_lock = None
-        self.last_num_sent = 0
-        self.last_num_recv = None
+        self.threading_lock = threading.Lock()
+        self.last_num_sent = 0 #TODO potentially remove this
+        self.last_num_recv = None #TODO potentially remove this
+        self.requested_sequence_length = None
+        self.queued_uint32_numbers = None
+        self.sent_uint32_numbers = None
 
-    def detect_heartbeat(self):
-        """
-        test
-        """
