9cc4e18e2af66bfc8e237d5874c36854a479463e
skiss10 <31713441+skiss10@users.noreply.github.com>
2023-04-03 12:36:02 -0700

    cleaned constants.py

--- constants.py
+++ constants.py
@@ -8,13 +8,13 @@ Date: 01/23/2023
 # This variable is a global variable for controlling the logging level
 LOG_LEVEL = 'DEBUG'
 
-# This variable is a global variable for the path of the log file 
+# This variable is a global variable for the path of the log file
 LOG_FILE_PATH = './app.log'
 
 # Set heartbeat interval in seconds
 HEARTBEAT_INTERVAL = 5
 
-# set reconnect window in seconds 
+# set reconnect window in seconds
 # after which reconnection attemps are rejected and server removes connection info from session_storage
 RECONNECT_WINDOW = 60
 
