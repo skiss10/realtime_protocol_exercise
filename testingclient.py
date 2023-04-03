f714f999002bdce5a69319666bec41cd441f8126
skiss10 <31713441+skiss10@users.noreply.github.com>
2023-04-03 10:45:27 -0700

    added loogging

--- testingclient.py
+++ testingclient.py
@@ -7,6 +7,7 @@ import threading
 import time
 import uuid
 import pickle
+import logging
 
 from constants import HEARTBEAT_INTERVAL, LOG_LEVEL, LOG_FILE_PATH
 from utils.message_sender import send_message
@@ -23,19 +24,22 @@ def end_connection(connection):
         if not connection.connection_thread_stopper.is_set():
             connection.connection_thread_stopper.set()
             print("threads for connection are flagged to stop")
+            logging.info(f"{CLIENT_NAME} Client - Connection - Threads for connection {connection.id} to server are flagged to stop")
 
-    except OSError:
+    except OSError as error:
         print("Error stopping the connection's theads")
-
+        logging.error(f"{CLIENT_NAME} Client - Connection - Error {error} stopping the theads for {connection.id}")
     # close the connection's socket
     try:
         if connection.state != "closed":
             connection.state = "closed"
             connection.conn.close()
             print("socket for connection closed")
+            logging.info(f"{CLIENT_NAME} Client - Connection - Connection {connection.id} stopped")
 
-    except OSError:
+    except OSError as error:
         print("Error closing the connections socket")
+        logging.error(f"{CLIENT_NAME} Client - Connection - Error {error} closing socket for {connection.id}")
 
 def connection_handler(connection):
     """
@@ -48,10 +52,12 @@ def connection_handler(connection):
     # spawn a new thread to handle inbound messages from server
     inbound_message_thread = threading.Thread(target=inbound_message_handler, args=(connection,))
     inbound_message_thread.start()
+    logging.debug(f"{CLIENT_NAME} Client - Connection - Started inbound_message_handler thread for connection {connection.id}")
 
     # spawn a new thread to check incoming heartbeats from server
     check_heartbeat_thread = threading.Thread(target=check_heartbeat, args=(connection,))
     check_heartbeat_thread.start()
+    logging.debug(f"{CLIENT_NAME} Client - Connection - Started check_heartbeat thread for connection {connection.id}")
 
     inbound_message_thread.join()
     check_heartbeat_thread.join()
@@ -74,10 +80,12 @@ def inbound_message_handler(connection):
             # check if the messages is a heartbeat
             if unserialized_message.name == "Heartbeat":
                 print(f"Received heartbeat at {time.time()}")
+                logging.info(f"{CLIENT_NAME} Client - Message - Received heartbeat over {connection.id}")
 
                 # send heartbeat_ack back to server
                 send_message(connection.conn, "Heartbeat_ack", "Heartbeat_ack", CLIENT_NAME)
                 print("Sent Heartbeat_ack")
+                logging.info(f"{CLIENT_NAME} Client - Message - Sent heartbeat_ack over {connection.id}")
 
                 # update last heartbeat timestamp
                 connection.last_heartbeat_ack = time.time()
@@ -86,6 +94,8 @@ def inbound_message_handler(connection):
             elif unserialized_message.name == "Greeting_ack":
                 print("Received Greeting_ack")
                 print("connection_id from server is %s", unserialized_message.data)
+                logging.info(f"{CLIENT_NAME} Client - Message - Recieved Greeting_ack over {connection.id}")
+                logging.info(f"{CLIENT_NAME} Client - Message - Connection_id from server is {unserialized_message.data}")
 
                 # assign connection id
                 connection.id = unserialized_message.data
@@ -93,15 +103,19 @@ def inbound_message_handler(connection):
             elif unserialized_message.name == "Data":
                 connection.last_num_recv = unserialized_message.data
                 print("recieved message from server with payload: %s", unserialized_message.data)
+                logging.info(f"{CLIENT_NAME} Client - Message - Recieved message from server with payload: {unserialized_message.data}")
+
 
         # trigger error when thread can't read from socket
-        except OSError:
-            print("Error reading from Socket")
+        except OSError as error:
+            print("Error reading from Socket. Suspending inbound_message_handler")
+            logging.error(f"{CLIENT_NAME} Client - Message - Error {error} reading message from server. Suspending inbound_message_handler")
             break
 
         # trigger error when incomplete message arrives from peer due to disruption / failure
-        except EOFError:
+        except EOFError as error:
             print("Error reading from Socket. Suspending inbound_message_handler")
+            logging.error(f"{CLIENT_NAME} Client - Message - Error {error} reading message from server. Suspending inbound_message_handler")
             break
 
 def attempt_reconnection(connection):
@@ -116,6 +130,7 @@ def attempt_reconnection(connection):
     # continue to try reconnection every 10 seconds
     except OSError:
         print("unable to reconnect to server, trying again in 10 seconds...")
+        logging.info(f"{CLIENT_NAME} Client - Reconnect - Unable to reconnect to server, trying again in 10 seconds...")
         time.sleep(10)
         attempt_reconnection(connection)
 
@@ -135,11 +150,11 @@ def check_heartbeat(connection):
 
             # handle missed heartbeats
             print("Heartbeats not recieved from server. Disconnecting from server...")
+            logging.info(f"{CLIENT_NAME} Client - Heartbeats - Heartbeats not recieved from server. Suspending check_heartbeat and Dsconnecting from server")
             end_connection(connection)
 
             # start attempting to reconnect to server
             attempt_reconnection(connection)
-
             break
 
         time.sleep(1)
@@ -153,9 +168,10 @@ def server_handler(peer_address, former_connection = None):
         try:
             conn.connect(peer_address)
             print("Connected to server")
+            logging.info(f"{CLIENT_NAME} Client - Connection - Connected to Server at {peer_address}")
 
             # instantiate connection object
-            connection = Connection(conn)
+            connection = Connection(conn, peer_address)
             
             # set connection object peer address tuple
             connection.addr = peer_address
@@ -164,11 +180,13 @@ def server_handler(peer_address, former_connection = None):
                 # send gretting message to peer
                 send_message(connection.conn, "Greeting", "", CLIENT_NAME)
                 print("sent greeting message")
+                logging.info(f"{CLIENT_NAME} Client - Message - Sent greeting message to Server at {peer_address}")
 
             else:
                 # send reconnect message to peer with previous connection_id and last message recieved
                 send_message(connection.conn, "reconnect_attempt", (former_connection.id, former_connection.last_num_recv), CLIENT_NAME)
                 print("sent reconnection message")
+                logging.info(f"{CLIENT_NAME} Client - Message - Sent reconnection message to Server at {peer_address} with info in tuple of {former_connection.id} and {former_connection.last_num_recv}")
 
             # define variable to stop threads associated with peer connection
             connection.connection_thread_stopper = threading.Event()
@@ -177,6 +195,7 @@ def server_handler(peer_address, former_connection = None):
             connection_handler_thread = threading.Thread(target=connection_handler, args=(connection,))
             connection_handler_thread.start()
             connection_handler_thread.join()
+            logging.debug(f"{CLIENT_NAME} Client - Started connection_handler thread for connection {connection.id}")
 
         # stop threads and close connection if keyboard interrupt
         except KeyboardInterrupt:
@@ -200,7 +219,19 @@ def main():
 
     except OSError:
         print("Unable to connect to server. Has the Server and/or proxy server been started?")
+        logging.info(f"{CLIENT_NAME} Client - System - Unable to connect to server. Has the Server and/or proxy server been started?")
 
 if __name__ == "__main__":
+    
+    # print client name to console
     print(CLIENT_NAME)
+
+    # Configure logging
+    logging.basicConfig(
+        level=LOG_LEVEL,
+        filename=LOG_FILE_PATH,
+        format='[%(asctime)s] %(levelname)s: %(message)s',
+        datefmt='%m-%d %H:%M:%S'
+        )
+    # start client main
     main()
