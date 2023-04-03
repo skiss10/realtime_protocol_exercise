018328afbd4f6d14ec41e09e22c84baa0ad0f136
skiss10 <31713441+skiss10@users.noreply.github.com>
2023-04-03 12:01:39 -0700

    updated constants

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 """
-Module to store all constant
+Module to store all constants
 
 Author: Stephen Kiss (stephenkiss986@gmail.com)
 Date: 01/23/2023
@@ -14,5 +14,7 @@ LOG_FILE_PATH = './app.log'
 # Set heartbeat interval in seconds
 HEARTBEAT_INTERVAL = 5
 
-# set reconnect window in seconds (after which reconnection attemps are rejected)
-RECONNECT_WINDOW = 240
+# set reconnect window in seconds 
+# after which reconnection attemps are rejected and server removes connection info from session_storage
+RECONNECT_WINDOW = 60
+
