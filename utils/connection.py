2220d367d0f34ffa9ff85c6b905e48f90ab8de46
skiss10 <31713441+skiss10@users.noreply.github.com>
2023-04-03 14:02:33 -0700

    cleaned connection.py

--- utils/connection.py
+++ utils/connection.py
@@ -22,11 +22,11 @@ class Connection:
         self.state = None # initial_connection, disconnected, reconnected
         self.client_id = None
         self.threading_lock = threading.Lock()
-        self.last_num_sent = 0 #TODO potentially remove this
-        self.last_num_recv = None #TODO potentially remove this
-        self.requested_sequence_length = None
+        self.sequence_length = None
         self.queued_uint32_numbers = None
         self.sent_uint32_numbers = None
         self.uint32_numbers_recieved = None
         self.continue_stream_from = None
+        self.last_num_sent = None #TODO potentially remove this
+        self.last_num_recv = None #TODO potentially remove this
 
