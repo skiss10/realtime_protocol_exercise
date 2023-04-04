36cb7c33db1067955a0ec3d058b0712c19e665fc
skiss10 <31713441+skiss10@users.noreply.github.com>
2023-04-04 09:47:16 -0700

    removed unused message object logic

--- utils/message.py
+++ utils/message.py
@@ -10,22 +10,3 @@ class Message:
         self.name = name
         self.data = data
         self.sender_id = sender_id
-
-    # def serialize(self, message):
-    #     """
-    #     Serialize a message
-    #     """
-
-    #     #serialize checksum message
-    #     serialized_message = pickle.dumps(message)
-    #     return serialized_message
-
-    # def deserialize(self, serialized_message):
-    #     """
-    #     Deserialize a message
-    #     """
-
-    #     #deserialize checksum message
-    #     deserialized_message = pickle.loads(serialized_message)
-    #     return deserialized_message
-    
