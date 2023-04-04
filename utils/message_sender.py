32251a9c170d2826d41e8c27d94ee1f41bfa5af0
skiss10 <31713441+skiss10@users.noreply.github.com>
2023-04-04 09:47:25 -0700

    added comments

--- utils/message_sender.py
+++ utils/message_sender.py
@@ -5,16 +5,17 @@ Exception Handling module
 import pickle
 from utils.message import Message
 
-def send_message(conn, msg_name, msg_data, msg_senderId):
+def send_message(conn, msg_name, msg_data, sender_id):
     """
     Utility function to send messages over a connection
     """
 
-    message = Message(msg_name, msg_data, msg_senderId)
+    # create message object with input params
+    message = Message(msg_name, msg_data, sender_id)
 
+    # serialize the message object
     serialized_message = pickle.dumps(message)
 
+    # sent the serialized message over the provided connection socket
     conn.sendall(serialized_message)
-
-    return
 
