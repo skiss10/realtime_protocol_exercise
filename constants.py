1e4dfd72452101cab3c7adfb8e82949bfaa7a93d
skiss10 <31713441+skiss10@users.noreply.github.com>
2023-04-04 14:42:08 -0700

    cleaned constants

--- constants.py
+++ constants.py
@@ -5,8 +5,8 @@ Author: Stephen Kiss (stephenkiss986@gmail.com)
 Date: 01/23/2023
 """
 
-# This variable is a global variable for controlling the logging level
-LOG_LEVEL = 'INFO' # Options: 'DEBUG' , 'INFO'
+# This variable is a global variable for controlling the logging level - Options: 'DEBUG' , 'INFO'
+LOG_LEVEL = 'DEBUG'
 
 # This variable is a global variable for the path of the log file
 LOG_FILE_PATH = './app.log'
