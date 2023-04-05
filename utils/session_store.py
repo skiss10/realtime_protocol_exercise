bddd865d85a2cf969cb5e0f0477da99699caf856
skiss10 <31713441+skiss10@users.noreply.github.com>
2023-04-04 21:03:55 -0700

    cleaned up InMemoryStore session store

--- utils/session_store.py
+++ utils/session_store.py
@@ -24,12 +24,10 @@ class InMemoryStore(AbstractSessionStore):
 
     def get(self, key):
         return self.store.get(key)
-    
+
     def set(self, key, value):
         self.store[key] = value
-        # logging.debug("[%s] Server - Storage - Setting InMemory data store %s:%s", SERVER_NAME, key, value)
-    
+
     def delete(self, key):
         if key in self.store:
             del self.store[key]
-            # logging.debug("[%s] Server - Storage - Deleting InMemory data store for %s", SERVER_NAME, key)
